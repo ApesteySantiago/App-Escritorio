2d047190c0aae2b12be926d51d7e875a69be49b7
ApesteySantiago <apesteysantiago@hotmail.com>
2023-02-03 12:21:53 -0300

    Add exercises

---
+++ 02__Tipos_de_datos_I/Ejercicios_UN2/Ejercicio_3_UN2.py
@@ -0,0 +1,10 @@
+"""
+Ejercicio 3
+
+Tome dos valores por consola, y guarde en una lista:
+
+[primer_valor, segundo_valor, la_suma_de_los_valores]
+
+Presente el resultado en la terminal del editor.
+
+"""
---
+++ 02__Tipos_de_datos_I/Ejercicios_UN2/Ejercicio_5_UN2.py
@@ -0,0 +1,10 @@
+"""
+Ejercicio 5
+
+Realice nuevamente los ejercicios de la unidad 1 (3, 4 y 5),
+pero tratando de utilizar una función, de forma que la operación 
+se realice dentro ded la misma.
+
+Presente el resultado en la terminal del editor.
+
+"""
