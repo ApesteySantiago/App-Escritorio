773e04893f437e76f0199e721b0c4e28d87d243a
ApesteySantiago <apesteysantiago@hotmail.com>
2023-02-21 12:24:49 -0300

    -

--- 06__Formatos_de_archivos_y_bases_de_datos_I/Ejercicios_Clases_Pregrabadas/mysql_crud/crearbd.py
+++ 06__Formatos_de_archivos_y_bases_de_datos_I/Ejercicios_Clases_Pregrabadas/mysql_crud/crearbd.py
@@ -6,11 +6,7 @@ pip install mysql-connector-python
 """
 import mysql.connector
 
-mibase = mysql.connector.connect(
-    host="localhost",
-    user="root",
-    password=""
-)
+mibase = mysql.connector.connect(host="localhost", user="root", password="")
 
 micursor = mibase.cursor()
 micursor.execute("CREATE DATABASE mi_plantilla2")
